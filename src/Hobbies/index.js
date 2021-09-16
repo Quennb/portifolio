7a22fead4a41a1a48696ae677778370a00e5dc54
Brenda de Oliveira <brealves35@gmail.com>
2021-09-15 21:49:10 -0300

    projeto

---
+++ src/Hobbies/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./hobbies.css";
+
+export default function Hobbies() {
+  return (
+    <>
+      {" "}
+      <div className="content">
+        <h2>Apresentação</h2>
+
+        <p id="missao">
+          Meu nome é{" "}
+          <em>
+            <strong>Brenda de Oliveira Alves</strong>
+          </em>{" "}
+          e eu estou cursando o terceiro ano do ensino médio no UNASP que possui
+          o curso de T.I implementado
+        </p>
+        <p id="missao">
+          Moro com a minha mãe, meu padastro e meu irmão mais novo
+        </p>
+        <p id="missao">
+          <em></em>Meu sonho é trabalhar e ter dinheiro o suficiente para viver
+          sozinha, auxiliar a minha mãe e ter uma vida boa e tranquila
+        </p>
+        <p id="missao">
+          <em></em>Minha maior dificuldade é decidir o que fazer na faculdade,
+          por esse motivo eu prefiro fazer um curso que forneça mais opções de
+          emprego no futuro
+        </p>
+      </div>
+    </>
+  );
+}
